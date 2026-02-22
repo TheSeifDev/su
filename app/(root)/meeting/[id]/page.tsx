9e9a1bc085ab48b13b5ef2f511b1cada90c2c820
Seif Ayman <167328383+S3fpl@users.noreply.github.com>
2026-02-22 16:15:22 +0200

    feat: implement MeetingRoom and MeetingSetup components, add useGetCallById hook for call management

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+'use client'
+import MeetingRoom from '@/components/MeetingLayout/MeetingRoom';
+import MeetingSetup from '@/components/MeetingLayout/MeetingSetup';
+import { useGetCallById } from '@/hooks/useGetCallById';
+import { useUser } from '@clerk/nextjs';
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import { Loader2 } from 'lucide-react';
+import React, { useState } from 'react'
 
-const Meeting = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const { id } = await params;
+const Meeting = ({ params }: { params: { id: string } }) => {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  const { call, isCallLoading } = useGetCallById(params.id);
+
+  if (!isLoaded || isCallLoading) return <Loader2 className="size-10 animate-spin text-blue-500" />
+
 
   return (
-    <div>Meeting Room: {id}</div>
+    <main className='w-full h-screen'>
+      <StreamCall call={call} >
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   )
 }
 
